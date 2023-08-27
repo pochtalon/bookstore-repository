e2e57e5a8b6cfd9700374f3135b3d9958316e487
pochtalon <pochtalon1010@gmail.com>
2023-08-27 17:15:08 +0300

    Added Pageable object to parameter of methods; Added dependency for open API to pom.xml; Added annotations for open API;

--- intro/src/main/java/mate/academy/intro/service/BookServiceImpl.java
+++ intro/src/main/java/mate/academy/intro/service/BookServiceImpl.java
@@ -11,6 +11,7 @@ import mate.academy.intro.model.Book;
 import mate.academy.intro.repository.book.BookRepository;
 import mate.academy.intro.repository.book.BookSpecificationBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -42,8 +43,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
