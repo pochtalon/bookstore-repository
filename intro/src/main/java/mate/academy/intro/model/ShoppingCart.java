ba998f616eb58258256564dd0490a2163c8b5f41
pochtalon <pochtalon1010@gmail.com>
2023-09-06 16:19:06 +0300

    Created models ShoppingCart and CartItem; Created yaml files for added shopping_carts and cart_items tables to DB;

---
+++ intro/src/main/java/mate/academy/intro/model/ShoppingCart.java
@@ -0,0 +1,36 @@
+package mate.academy.intro.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Set;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @Column(name = "user_id", nullable = false)
+    private User user;
+    @OneToMany
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<CartItem> cartItems;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+}
