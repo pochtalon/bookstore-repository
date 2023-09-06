bf291766c833a53498d421537be3a7fc5417c1d8
pochtalon <pochtalon1010@gmail.com>
2023-09-06 14:32:41 +0300

    Added Set<Long> categories for adding data to books_categories table when the book is created; Changed save() method in BookServiceImpl for parsing Set<Category> during the book creation;

--- intro/src/main/java/mate/academy/intro/service/impl/BookServiceImpl.java
+++ intro/src/main/java/mate/academy/intro/service/impl/BookServiceImpl.java
@@ -10,8 +10,10 @@ import mate.academy.intro.dto.book.CreateBookRequestDto;
 import mate.academy.intro.exception.EntityNotFoundException;
 import mate.academy.intro.mapper.BookMapper;
 import mate.academy.intro.model.Book;
+import mate.academy.intro.model.Category;
 import mate.academy.intro.repository.book.BookRepository;
 import mate.academy.intro.repository.book.BookSpecificationBuilder;
+import mate.academy.intro.repository.category.CategoryRepository;
 import mate.academy.intro.service.BookService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -21,12 +23,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final CategoryRepository categoryRepository;
     private final BookMapper bookMapper;
     private final BookSpecificationBuilder builder;
 
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
         Book book = bookMapper.toModel(requestDto);
+        if (requestDto.getCategories() != null && requestDto.getCategories().size() > 0) {
+            for (Long categoryId : requestDto.getCategories()) {
+                Optional<Category> category = categoryRepository.findById(categoryId);
+                category.ifPresent(value -> book.getCategories().add(value));
+            }
+        }
         book.setId(bookRepository.save(book).getId());
         return bookMapper.toDto(book);
     }
