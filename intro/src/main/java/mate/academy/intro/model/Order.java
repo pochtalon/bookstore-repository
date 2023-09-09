89fc8262fc8c414cc9c27b153da1959ae7f85eca
pochtalon <pochtalon1010@gmail.com>
2023-09-09 12:42:15 +0300

    Created tables orders and order_items through liquibase;

--- intro/src/main/java/mate/academy/intro/model/Order.java
+++ intro/src/main/java/mate/academy/intro/model/Order.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -30,12 +31,11 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @ToString.Exclude
-    @EqualsAndHashCode.Exclude
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
     private User user;
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private Status status = Status.PENDING;
     @Column(nullable = false)
     private BigDecimal total;
     @Column(name = "order_date", nullable = false)
@@ -47,7 +47,7 @@ public class Order {
     @EqualsAndHashCode.Exclude
     private Set<OrderItem> orderItems;
     @Column(name = "is_deleted", nullable = false)
-    private boolean isDeleted;
+    private boolean isDeleted = false;
 
     public enum Status {
         PENDING,
