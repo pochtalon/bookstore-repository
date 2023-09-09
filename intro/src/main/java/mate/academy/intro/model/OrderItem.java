45cb360287fd335243e3952e9f391861bc641022
pochtalon <pochtalon1010@gmail.com>
2023-09-09 11:52:45 +0300

    Created Order and OrderItem models;

--- intro/src/main/java/mate/academy/intro/model/OrderItem.java
+++ intro/src/main/java/mate/academy/intro/model/OrderItem.java
@@ -8,27 +8,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
 @Data
-@SQLDelete(sql = "UPDATE cart_items SET is_deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE order_items SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
-@Table(name = "cart_items")
-public class CartItem {
+@Table(name = "order_items")
+public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name = "shopping_cart_id", nullable = false, referencedColumnName = "id")
-    private ShoppingCart shoppingCart;
+    @JoinColumn(name = "order_id", nullable = false, referencedColumnName = "id")
+    private Order order;
     @ManyToOne
     @JoinColumn(name = "book_id", nullable = false, referencedColumnName = "id")
     private Book book;
     @Column(nullable = false)
     private int quantity;
+    @Column(nullable = false)
+    private BigDecimal price;
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
 }
