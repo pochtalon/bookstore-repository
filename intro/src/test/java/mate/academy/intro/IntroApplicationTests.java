ad6372d828243133902feee18694617c84e91f15
pochtalon <pochtalon1010@gmail.com>
2023-09-13 15:09:55 +0300

    Added Docker dependencies; Fixed some issues;

--- intro/src/test/java/mate/academy/intro/IntroApplicationTests.java
+++ intro/src/test/java/mate/academy/intro/IntroApplicationTests.java
@@ -1,13 +1,10 @@
 package mate.academy.intro;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IntroApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+//    @Test
+//    void contextLoads() {
+//    }
 }
